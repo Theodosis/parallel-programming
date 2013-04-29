204b37b9b2317d97c0af4f035f57f05cffba2908
Theodosis Sourgkounis <thsourg@gmail.com>
2013-04-29 12:41:27 +0300

    ssl support

--- traveller-salesman-problem/index.php
+++ traveller-salesman-problem/index.php
@@ -194,7 +194,7 @@
             <p> GO CANADA!!!</p>
             <div class="cc" style="text-align:center">
                 <a rel="license" href="http://creativecommons.org/licenses/by/2.0/">
-                    <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a>
+                    <img alt="Creative Commons License" style="border-width:0" src="//i.creativecommons.org/l/by/3.0/88x31.png" /></a>
                     <br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Generic License</a>.
             </div>
         </div>
